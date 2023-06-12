e56e58cd8c6613ccea4f5c04af6252eb4434b99c
Guilherme Camposo <guilherme.camposo@me.com>
2023-06-12 12:11:04 -0400

    adding channel-broker demo to automation

--- Eventing-demo/broker-demo/1-BrokerServices.sh
+++
@@ -1,3 +0,0 @@
-oc new-project eventing-demo
-kn service create invoice-service-display --image quay.io/openshift-knative/knative-eventing-sources-event-display --label app.kubernetes.io/part-of=broker-demo -n eventing-demo
-kn service create order-service-display --image quay.io/openshift-knative/knative-eventing-sources-event-display  --label app.kubernetes.io/part-of=broker-demo -n eventing-demo
